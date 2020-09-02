b4e716ce4184c1488d13296be4babc41fcd8f1ab
captain-blue210 <captain.blue210@gmail.com>
2020-09-03 00:14:15 +0900

    feat:convert image file from jpg to png.

--- kadai1/captainblue/imgconv/imgconv.go
+++ kadai1/captainblue/imgconv/imgconv.go
@@ -1,8 +1,13 @@
 package imgconv
 
 import (
+	//"image"
+	"fmt"
+	"image/jpeg"
+	"image/png"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func GetImgFiles(dirname string) ([]string, error) {
@@ -17,4 +22,35 @@ func GetImgFiles(dirname string) ([]string, error) {
 		return nil, err
 	}
 	return filenames, nil
+}
+
+func ConvertImgFIle(name string) error {
+	fmt.Println("画像の変換を開始します。")
+	// 変換する画像ファイルを読み込む
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// 画像オブジェクトに変換する
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		return err
+	}
+
+	// 出力先ファイルを用意する
+	path := strings.Replace(name, filepath.Ext(name), ".png", 1)
+	output, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	// 画像ファイルを出力する
+	err = png.Encode(output, img)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- kadai1/captainblue/main.go
+++ kadai1/captainblue/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"path/filepath"
+
 	"gopherdojo-studyroom/kadai1/captainblue/imgconv"
 )
 
@@ -11,15 +14,30 @@ var (
 )
 
 func init() {
-	flag.StringVar(&beforeExt, "b", ".jpg", "set image extension before converted")
-	flag.StringVar(&afterExt, "a", ".png", "set image extension after converted")
+	flag.StringVar(&beforeExt, "b", ".jpg", "変換前の画像の拡張子を指定してください。")
+	flag.StringVar(&afterExt, "a", ".png", "変換後の画像の拡張子を指定してください。")
 }
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	names, _ := imgconv.GetImgFiles(args[0])
+	if len(args) <= 0 {
+		log.Fatal("引数を指定してください。")
+	}
+
+	names, err := imgconv.GetImgFiles(args[0])
+	if err != nil {
+		log.Fatal("ファイルの取得に失敗しました。")
+	}
+
 	for _, name := range names {
-		fmt.Println(name)
+		if filepath.Ext(name) == ".jpg" {
+			err := imgconv.ConvertImgFIle(name)
+			if err != nil {
+				log.Fatal("画像の変換に失敗しました。")
+			} else {
+				fmt.Println("画像の変換が完了しました。")
+			}
+		}
 	}
 }
