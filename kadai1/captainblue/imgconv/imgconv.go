2edbcd020585952f1e4d0cfe3d3c105d58229f51
captain-blue210 <captain.blue210@gmail.com>
2020-09-13 22:38:41 +0900

    feat:support extension args

--- kadai1/captainblue/imgconv/imgconv.go
+++ kadai1/captainblue/imgconv/imgconv.go
@@ -1,8 +1,8 @@
 package imgconv
 
 import (
-	//"image"
 	"fmt"
+	"image"
 	"image/jpeg"
 	"image/png"
 	"os"
@@ -10,11 +10,22 @@ import (
 	"strings"
 )
 
-func GetImgFiles(dirname string) ([]string, error) {
+// Extension List
+const (
+	PNG  string = ".png"
+	JPG  string = ".jpg"
+	JPEG string = ".jpeg"
+)
+
+// GetImgSrcFiles : get image files from dir
+func GetImgSrcFiles(dirname string, beforeExt string) ([]string, error) {
 	var filenames []string
 	err := filepath.Walk(dirname,
 		func(path string, info os.FileInfo, err error) error {
-			filenames = append(filenames, path)
+			fmt.Println(filepath.Ext(path), beforeExt)
+			if filepath.Ext(path) == beforeExt {
+				filenames = append(filenames, path)
+			}
 			return nil
 		})
 
@@ -24,33 +35,55 @@ func GetImgFiles(dirname string) ([]string, error) {
 	return filenames, nil
 }
 
-func ConvertImgFIle(name string) error {
-	fmt.Println("画像の変換を開始します。")
-	// 変換する画像ファイルを読み込む
-	file, err := os.Open(name)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// 画像オブジェクトに変換する
-	img, err := jpeg.Decode(file)
-	if err != nil {
-		return err
-	}
-
+func convert(name string, afterExt string, img image.Image) error {
 	// 出力先ファイルを用意する
-	path := strings.Replace(name, filepath.Ext(name), ".png", 1)
+	path := strings.Replace(name, filepath.Ext(name), afterExt, 1)
 	output, err := os.Create(path)
 	if err != nil {
 		return err
 	}
 	defer output.Close()
 
-	// 画像ファイルを出力する
-	err = png.Encode(output, img)
+	switch afterExt {
+	case PNG:
+		if err := png.Encode(output, img); err != nil {
+			return fmt.Errorf("pngへの変換に失敗しました。%v", err)
+		}
+	case JPG, JPEG:
+		if err := jpeg.Encode(output, img, nil); err != nil {
+			return fmt.Errorf("jpegへの変換に失敗しました。%v", err)
+		}
+	}
+	return nil
+}
+
+func decodeFileToImg(name string) (image.Image, error) {
+	// 変換する画像ファイルを読み込む
+	file, err := os.Open(name)
 	if err != nil {
-		return err
+		return nil, err
+	}
+	defer file.Close()
+
+	// 画像オブジェクトに変換する
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
+
+// ConvertImgFIles : convert Image files
+func ConvertImgFIles(names []string, afterExt string) error {
+	for _, name := range names {
+		img, err := decodeFileToImg(name)
+		if err != nil {
+			return err
+		}
+
+		if err := convert(name, afterExt, img); err != nil {
+			return err
+		}
 	}
 	return nil
 }
--- kadai1/captainblue/main.go
+++ kadai1/captainblue/main.go
@@ -2,42 +2,34 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"path/filepath"
+	"os"
 
-	"gopherdojo-studyroom/kadai1/captainblue/imgconv"
+	"github.com/captain-blue210/gopherdojo-studyroom/kadai1/captainblue/imgconv"
 )
 
 var (
-	beforeExt, afterExt string
+	dirPath, beforeExt, afterExt string
 )
 
-func init() {
+func main() {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	flag.StringVar(&dirPath, "p", wd, "変換したい画像のディレクトリパスを指定してください。")
 	flag.StringVar(&beforeExt, "b", ".jpg", "変換前の画像の拡張子を指定してください。")
 	flag.StringVar(&afterExt, "a", ".png", "変換後の画像の拡張子を指定してください。")
-}
-
-func main() {
 	flag.Parse()
-	args := flag.Args()
-	if len(args) <= 0 {
-		log.Fatal("引数を指定してください。")
-	}
+	flag.Args()
 
-	names, err := imgconv.GetImgFiles(args[0])
+	names, err := imgconv.GetImgSrcFiles(dirPath, beforeExt)
 	if err != nil {
-		log.Fatal("ファイルの取得に失敗しました。")
+		log.Fatalf("ファイルの取得に失敗しました。 %v", err)
 	}
 
-	for _, name := range names {
-		if filepath.Ext(name) == ".jpg" {
-			err := imgconv.ConvertImgFIle(name)
-			if err != nil {
-				log.Fatal("画像の変換に失敗しました。")
-			} else {
-				fmt.Println("画像の変換が完了しました。")
-			}
-		}
+	if err := imgconv.ConvertImgFIles(names, afterExt); err != nil {
+		log.Fatalf("ファイルの変換に失敗しました。 %v", err)
 	}
 }
